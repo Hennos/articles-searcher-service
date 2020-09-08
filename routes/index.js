4e9d66fb671a7bc6f4b8b95011a381c79b77326f
Evgeny Karasev <gnomskg@gmail.com>
2020-09-08 16:54:22 +0300

    feat: remove view, view engine

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "articles-searcher-service" });
+router.get("/", function (req, res, next) {
+  res.send("articles-searcher-service");
 });
 
 module.exports = router;
